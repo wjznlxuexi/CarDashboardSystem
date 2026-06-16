f32460f7392f4adc983cbb2001046042e3feedb1
wjznlxuexi <1264549545@qq.com>
2026-06-16 11:18:21 +0800

    Update main.cpp

--- final_simulator/main.cpp
+++ final_simulator/main.cpp
@@ -1,11 +1,22 @@
 #include "simulatorwindow.h"
+#include "common/vehicledata.h"
+
 #include <QApplication>
+#include <QCoreApplication>
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    SimulatorWindow w;
-    w.show();
+    QApplication application(argc, argv);
 
-    return a.exec();
+    QCoreApplication::setOrganizationName("WuhanTextileUniversity");
+    QCoreApplication::setApplicationName("VehicleSignalSimulator");
+    QApplication::setFont(QFont("Microsoft YaHei", 10));
+
+    qRegisterMetaType<VehicleData>("VehicleData");
+
+    SimulatorWindow window;
+    window.show();
+
+    return application.exec();
 }
