280bf4734883cd3dac4dbd37d7b26eaa557ee28d
yzyysir <2549436874@qq.com>
2026-06-16 10:37:44 +0800

    Update turnindicatorwidget.h

--- dashboard/indicators/turnindicatorwidget.h
+++ dashboard/indicators/turnindicatorwidget.h
@@ -2,17 +2,36 @@
 #define TURNINDICATORWIDGET_H
 
 #include <QWidget>
+#include <QTimer>
+#include <QPixmap>
 
 class TurnIndicatorWidget : public QWidget
 {
     Q_OBJECT
-public:
-    explicit TurnIndicatorWidget(QWidget *parent = 0);
 
-signals:
+public:
+    explicit TurnIndicatorWidget(QWidget *parent = NULL);
 
 public slots:
+    void setTurnSignal(int signal);
+    void setHazardEnabled(bool enabled);
 
+private slots:
+    void updateBlinkState();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    int m_signal;
+    bool m_hazard;
+    bool m_blinkOn;
+    QTimer *m_blinkTimer;
+    QPixmap m_leftPixmap;
+    QPixmap m_rightPixmap;
+    QPixmap m_leftPixmapDimmed;
+    QPixmap m_rightPixmapDimmed;
 };
 
-#endif // TURNINDICATORWIDGET_H
+#endif
+
