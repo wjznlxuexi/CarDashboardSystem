c10f88969a1771fb35f86fa83b8ef2e34aa6fb41
jianghu031 <jianghu031@gmail.com>
2026-06-16 10:27:45 +0800

    Update networkclient.h

--- dashboard/network/networkclient.h
+++ dashboard/network/networkclient.h
@@ -2,17 +2,47 @@
 #define NETWORKCLIENT_H
 
 #include <QObject>
+#include <QTcpSocket>
+#include <QTimer>
+#include <QByteArray>
+#include "vehicledata.h"
 
-class networkclient : public QObject
+class NetworkClient : public QObject
 {
     Q_OBJECT
-public:
-    explicit networkclient(QObject *parent = 0);
 
-signals:
+public:
+    explicit NetworkClient(QObject *parent = NULL);
+    ~NetworkClient();
+
+    void connectToServer(const QString &host, quint16 port);
+    void disconnectFromServer();
 
 public slots:
+    void sendCommand(const QString &command);
 
+signals:
+    void vehicleDataChanged(const VehicleData &data);
+    void connectionStateChanged(bool connected);
+    void networkError(const QString &message);
+
+private slots:
+    void onConnected();
+    void onDisconnected();
+    void onReadyRead();
+    void onReconnectTimeout();
+
+private:
+    VehicleData parseJsonToVehicleData(const QByteArray &jsonData);
+    void processReceivedData();
+
+    QTcpSocket *m_socket;
+    QTimer *m_reconnectTimer;
+    QByteArray m_buffer;
+
+    QString m_host;
+    quint16 m_port;
+    bool m_intentionalDisconnect;
 };
 
-#endif // NETWORKCLIENT_H
+#endif
